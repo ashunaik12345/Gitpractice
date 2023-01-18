f3bf68da7ad5af5c79ce050d0b8e8bface64eb05
ASHWINI <ASHWINI@DESKTOP-28SANPA>
2023-01-18 12:31:55 +0530

    Frame work commited practice

---
+++ src/test/java/demo/CreateOrgTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+public void test1() {
+	System.out.println("create org");
+	System.out.println("git");
+}
+}
